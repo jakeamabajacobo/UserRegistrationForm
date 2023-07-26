51fc42b09563aca7162dbc09dcafd60429d2f85b
Jake Jacobo <jakejacobo2015@gmail.com>
2023-07-26 17:44:40 +0800

    Revert "test1234242"
    This reverts commit bd43c29f27cb75b9accbd2eaa024fb0b05edb456.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import bannerImage from './banner.png';
 
-const ApiData = () => {
-  const [data, setData] = useState([]);
+const UserRegistrationForm = async () => {
+  
+  const [professions, setProfessions] = useState([]);
+  const [profession, setProfession] = useState('');
 
-  useEffect(() => {
-    // Fetch data from the proxy server (serverless function)
-    axios.get('/api/rest/refdata/countries')
-      .then(response => {
-        setData(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
+  const rootUrl = process.env.NODE_ENV === "production" ? "https://test-services.interact.technology" : ""
+   
+    // fetch(`${rootUrl}/rest/refdata/professions`)
+    // .then((response) => console.log(response));
+          
+
+    const res = await fetch(`https://test-services.interact.technology/rest/refdata/professions`)
+   console.log(res)
+  
 
   return (
-    <div>
-      <h2>API Data:</h2>
-      <ul>
-        {data.map(item => (
-          <li key={item.isoCode}>{item.isoCode}</li>
-        ))}
-      </ul>
-    </div>
+
+    <><h1>test</h1>
+ 
+    {/* <select value={profession} onChange={(e) => setProfession(e.target.value)}>
+      <option  key="default" value="">Select Profession</option>
+      {professions.map((profession) => (
+        <option  key={profession.professionId}  value={profession.professionId}>{profession.professionName}</option>
+      ))}
+    </select> */}
+
+  
+    
+    </>
+
+
+
+ 
+
+
   );
 };
 
-export default ApiData
+export default UserRegistrationForm;
