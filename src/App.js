5e353baec409a35e45a52c2750719a74dcf360a6
Jake Jacobo <jakejacobo2015@gmail.com>
2023-07-26 15:53:33 +0800

    test2

--- src/App.js
+++ src/App.js
@@ -3,20 +3,46 @@ import bannerImage from './banner.png';
 
 const UserRegistrationForm = () => {
   
-  
-    const rootUrl = process.env.NODE_ENV === "production" ? "https://registeruser55knots.vercel.app" : ""
-   
-    fetch('/rest/refdata/professions')
-    .then((response) => 
-    
-    console.log(response)
-    );
+  const [professions, setProfessions] = useState([]);
+  const [profession, setProfession] = useState('');
 
+  const rootUrl = process.env.NODE_ENV === "production" ? "https://registeruser55knots.vercel.app" : ""
+   
+
+
+    fetch('${rootUrl}/rest/refdata/professions')
+    .then((response) => response.json())
+    .then((data) => setProfessions(data))
+    .catch((error) => console.error(error));
+
+
+    // fetch('/rest/refdata/professions')
+    // .then((response) => response.json())
+    // .then((data) => setProfessions(data))
+    // .catch((error) => console.error(error));
           
   
 
   return (
-<h1>test</h1>
+
+    <><h1>test</h1>
+ 
+    <select value={profession} onChange={(e) => setProfession(e.target.value)}>
+      <option  key="default" value="">Select Profession</option>
+      {professions.map((profession) => (
+        <option  key={profession.professionId}  value={profession.professionId}>{profession.professionName}</option>
+      ))}
+    </select>
+
+  
+    
+    </>
+
+
+
+ 
+
+
   );
 };
 
