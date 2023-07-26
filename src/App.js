7cde7793d37e907e24b689d4219c2bc2eb4cf864
Jake Jacobo <jakejacobo2015@gmail.com>
2023-07-26 16:30:00 +0800

    fdasfdsa
    sdafasd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import bannerImage from './banner.png';
 
-const UserRegistrationForm = () => {
+const UserRegistrationForm = async () => {
   
   const [professions, setProfessions] = useState([]);
   const [profession, setProfession] = useState('');
 
   const rootUrl = process.env.NODE_ENV === "production" ? "https://test-services.interact.technology" : ""
    
-
-
-    // fetch('/rest/refdata/professions')
-    // .then((response) => console.log('response: ' +response))
-    // .then((data) =>console.log('data: ' +data))
-    // .catch((error) => console.log('errror:  ' +error));
-
-
-    fetch(`${rootUrl}/rest/refdata/professions`)
-    .then((response) => console.log(response));
+    // fetch(`${rootUrl}/rest/refdata/professions`)
+    // .then((response) => console.log(response));
           
+
+    const res = await fetch(`https://test-services.interact.technology/rest/refdata/professions`)
+   console.log( res.json())
   
 
   return (
